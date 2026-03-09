41946e2f5c3aecd26768648bbb758384004d1578
FournierLyes <fournierlyes@outlook.fr>
2026-03-09 10:54:33 +0100

    Added Cpp09 structure; started researching the optimal uses of each containers

---
+++ Cpp09/ex00/sources/main.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfournie <lfournie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 10:49:45 by lfournie          #+#    #+#             */
+/*   Updated: 2026/03/09 10:52:56 by lfournie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/lib.hpp"
+
+//container to use: deque, may be needed for ex02. may use map container instead
+int main()
+{
+
+}
