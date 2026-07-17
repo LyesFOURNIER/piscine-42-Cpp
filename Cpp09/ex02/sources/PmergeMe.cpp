73b091a13d645fdf420817d9683e52625e496bf4
lfournie <lfournie@lfournie.myguest.virtualbox.org>
2026-07-17 15:13:17 +0200

    Cpp09/ex02 merge sort should be finished

--- Cpp09/ex02/sources/PmergeMe.cpp
+++ Cpp09/ex02/sources/PmergeMe.cpp
@@ -56,33 +56,23 @@ void	vectorMerge(std::vector<int> *v, size_t *order)
 {
 	int	tmp;
 
-	std::cout 	<< "order = " << *order
-				<< "\nv->size() / 2 = " << v->size() / 2 << std::endl;
-	if (*order >= v->size() / 2)
+	std::cout 	<< "\norder = " << *order
+				<< std::endl;
+	if (*order > v->size() / 2)
 		return;
 
 	for (size_t groups = 1; groups <  v->size() / *order; groups += 2)
 	{
-		std::cout << "\ngroups = [ " << groups << " ]\n";
 		if ((*v)[*order * groups - 1] > (*v)[*order * (groups + 1) - 1])
 		{
-			std::cout	<< "left group last elem: " << (*v)[*order * groups - 1]
-						<< "\nright group last elem: " << (*v)[*order * (groups + 1) - 1]
-						<< std::endl;
 			for (size_t i = *order * groups - *order; i < *order * groups; i++)
 			{
 				tmp = (*v)[i];
-				std::cout	<< "\ntmp = " << tmp
-							<< "\n(*v)[i] before change = " << (*v)[i];
 				(*v)[i] = (*v)[i + *order];
-				std::cout	<< "\n(*v)[i] after change = " << (*v)[i]
-							<< "\n(*v)[i + *order] before change = " << (*v)[i + *order];
 				(*v)[i + *order] = tmp;
-				std::cout << "\n(*v)[i + *order] after change = " << (*v)[i + *order];
 			}
 		}
 	}
-	std::cout << std::endl;
 	*order *= 2;
 	for (std::vector<int>::iterator it = v->begin(); it != v->end(); it++)
 	{
@@ -104,7 +94,18 @@ void	PmergeMe::vectSort()
 	std::cout << std::endl;
 
 	vectorMerge(&v, &order);
-	
+
+	std::cout << "\norder after vectorMerge: " << order << "\ninitial sequence: ";
+	for (std::vector<int>::iterator it = _seq.begin(); it != _seq.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout <<"\nvector v after vectorMerge: ";
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
 
 void	dequeMerge(std::vector<int> *d, size_t *order)
